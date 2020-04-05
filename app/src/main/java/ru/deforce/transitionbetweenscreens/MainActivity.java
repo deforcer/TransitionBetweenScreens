4277da3315658448342720390281fa58edb19204
Alexey Dmitriev <alexey@dmitriev.info>
2020-04-05 12:16:44 +0300

    task done

--- app/src/main/java/ru/deforce/transitionbetweenscreens/MainActivity.java
+++ app/src/main/java/ru/deforce/transitionbetweenscreens/MainActivity.java
@@ -2,13 +2,45 @@ package ru.deforce.transitionbetweenscreens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text1;
+    private String rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        rnd = Integer.toString((int) (Math.random() * 100));
+        text1.setText(getString(R.string.url, rnd));
+
     }
+
+    public void initViews() {
+        text1 = findViewById(R.id.textView);
+
+    }
+
+    public void onPrevClick(View v) {
+        finish();
+
+    }
+
+    public void onNextClick(View v) {
+
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(intent);
+
+
+    }
+
+
 }
